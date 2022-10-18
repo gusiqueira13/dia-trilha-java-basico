edcd8c994536ac95b4d7ce2d225778d843bc46a4
Gustavo Siqueira <99092902+gusiqueira13@users.noreply.github.com>
2022-10-18 18:18:24 -0300

    commit do conteudo

--- TIPOS-VARIAVEIS/src/TiposVariaveis.java
+++ TIPOS-VARIAVEIS/src/TiposVariaveis.java
@@ -1,5 +1,15 @@
+import java.sql.CallableStatement;
+
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        int exemploLegal = 2;
+
+        exemploLegal = 8;
+
+        String NAO_MUDARA = "casa";
+
+        System.out.print(exemploLegal, NAO_MUDARA);
+
     }
 }
